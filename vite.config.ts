547f481ee6e64c4db9f93890dcf510cbf882a37c
nino <dankozero111@gmail.com>
2024-12-06 18:53:06 +0300

    [build]: basic config

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@store': path.resolve(__dirname, 'src/app/store'),
+      '@type': path.resolve(__dirname, 'src/types'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@widgets': path.resolve(__dirname, 'src/widgets'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@entities': path.resolve(__dirname, 'src/entities'),
+      '@shared': path.resolve(__dirname, 'src/shared'),
+    },
+  },
+});
