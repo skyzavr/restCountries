44dee9654d95d90015b95cb47038ef4370e6fd5a
skyzavr <dankozero111@gmail.com>
2025-02-06 11:33:13 +0300

    [feat]: add main page country card component

---
+++ src/entities/countryCard/model/types.ts
@@ -0,0 +1,12 @@
+type typeEnumeration = { [key: string]: string };
+
+export type Country = {
+  name: typeEnumeration;
+  capital: string[];
+  languages: typeEnumeration;
+  borders: string[];
+  maps: typeEnumeration;
+  population: number;
+  continents: string[];
+  flags: typeEnumeration;
+};
---
+++ src/entities/countryCard/ui/CountryCard.tsx
@@ -0,0 +1,36 @@
+import { convertListToStr } from '@shared/lib/stringHelpers';
+import { Country } from '../model/types';
+
+import css from './countryCard.module.css';
+
+type cardProps = { data: Country };
+
+export const CountryCard = ({ data }: cardProps) => {
+  const { name, capital, population, continents, flags } = data;
+  const { svg, alt } = flags;
+
+  const infoList = [
+    { key: 'Population', value: Number(population).toLocaleString() },
+    { key: 'Region', value: convertListToStr(continents) },
+    { key: 'Capital', value: convertListToStr(capital) },
+  ];
+
+  return (
+    <div className={css.wrapper}>
+      <div className={css.flag}>
+        <img src={svg} alt={alt} />
+      </div>
+      <div className={css.info}>
+        <div className={css.countryName}>{name.common}</div>
+        <div className={css.infoWrapper}>
+          {infoList.map(({ key, value }, ind) => (
+            <div className={css.infoItem} key={ind}>
+              <span className={css.listTitle}>{key}</span>
+              <span className={css.listValue}>{value}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
