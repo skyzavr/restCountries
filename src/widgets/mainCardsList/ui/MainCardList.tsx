c1c14b1e0118d63325e7f6b089e7a73a779a2bb7
skyzavr <dankozero111@gmail.com>
2025-02-24 16:12:32 +0300

    [feat]: shows data depends on status (loading:skeleton, error: error message, success: list of cards)

--- src/widgets/mainCardsList/ui/MainCardList.tsx
+++ src/widgets/mainCardsList/ui/MainCardList.tsx
@@ -1,14 +1,40 @@
-import { Country } from '@entities/countryCard/model/types';
-import { CountryCard } from '@entities/countryCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { useCallback, useEffect } from 'react';
 
+import { appDispatch, RootState } from '@app/store/store';
+import { CountryCard } from '@entities/countryCard';
+import { CardListError } from '@entities/cardListError';
+import { CardListSkeleton } from '@entities/cardListSkeleton';
+
+import { fetchCountries } from '../model/slice';
 import css from './mainCardList.module.css';
 
-type cardListProps = { data: Country[] };
+export const MainCardList = () => {
+  const dispatch = useDispatch<appDispatch>();
 
-export const MainCardList = ({ data }: cardListProps) => {
+  const { countries, status, error } = useSelector(
+    (state: RootState) => state.countries
+  );
+  const { region: filter } = useSelector(
+    (state: RootState) => state.mainPageFilter
+  );
+  const { query } = useSelector((state: RootState) => state.mainPageSearch);
+
+  const data = useCallback(
+    () => dispatch(fetchCountries({ query, filter })),
+    [dispatch, query, filter]
+  );
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    data();
+  }, [data]);
+
+  if (status === 'loading') return <CardListSkeleton />;
+  if (status === 'failed') return <CardListError error={error} />;
   return (
     <div className={css.wrapper}>
-      {data.map((country, ind) => (
+      {countries.slice(0, 12).map((country, ind) => (
         <CountryCard data={country} key={ind} />
       ))}
     </div>
