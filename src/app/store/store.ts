3b583d5ce9007f81971db399c0c62c4ba9c8db3b
skyzavr <dankozero111@gmail.com>
2025-02-24 16:00:28 +0300

    [feat]: add countrues reducer

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,11 +1,13 @@
 import { setRegionReducer } from '@features/mainPageFilter/model/slice';
 import { searchReducer } from '@features/mainPageSearch/model/slice';
 import { configureStore } from '@reduxjs/toolkit';
+import { fetchCountriesReducer } from '@widgets/mainCardsList/model/slice';
 
 export const store = configureStore({
   reducer: {
     mainPageSearch: searchReducer,
     mainPageFilter: setRegionReducer,
+    countries: fetchCountriesReducer,
   },
 });
 
