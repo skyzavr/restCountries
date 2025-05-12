28119be4e369ce9f4ab0d2f73a6b5fe3826de78d
skyzavr <dankozero111@gmail.com>
2025-05-12 12:47:12 +0300

    [feat]: add constant data

--- src/widgets/mainCardsList/model/data.ts
+++ src/widgets/mainCardsList/model/data.ts
@@ -1,1 +1,11 @@
+import { InterObserverOptions } from '@shared/model/type';
+
 export const baseUrl = 'https://restcountries.com/v3.1';
+
+export const options: InterObserverOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5,
+};
+
+export const countriesPerPage = 10;
