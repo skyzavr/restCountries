18d1b28fa03068420c345872c79fdfd272019b21
skyzavr <dankozero111@gmail.com>
2025-05-12 12:46:38 +0300

    [feat]: add cards lazy loading with useObserver hook

--- src/widgets/mainCardsList/ui/MainCardList.tsx
+++ src/widgets/mainCardsList/ui/MainCardList.tsx
@@ -1,15 +1,19 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useCallback, useEffect } from 'react';
+import { RefObject, useCallback, useEffect, useState } from 'react';
 
 import { appDispatch, RootState } from '@app/store/store';
 import { CountryCard } from '@entities/countryCard';
 import { CardListError } from '@entities/cardListError';
 import { CardListSkeleton } from '@entities/cardListSkeleton';
+import { useObserver } from '@shared/lib/useObserver';
 
 import { fetchCountries } from '../model/slice';
+import { countriesPerPage, options } from '../model/data';
 import css from './mainCardList.module.css';
 
 export const MainCardList = () => {
+  const [node, isIntersecting] = useObserver(options);
+  const [cardsNumber, setCardsNumber] = useState(countriesPerPage);
   const dispatch = useDispatch<appDispatch>();
 
   const { countries, status, error } = useSelector(
@@ -20,23 +24,40 @@ export const MainCardList = () => {
   );
   const { query } = useSelector((state: RootState) => state.mainPageSearch);
 
-  const data = useCallback(
-    () => dispatch(fetchCountries({ query, filter })),
-    [dispatch, query, filter]
-  );
+  const data = useCallback(async () => {
+    await dispatch(fetchCountries({ query, filter }));
+    setCardsNumber(countriesPerPage);
+  }, [dispatch, query, filter]);
+
+  const calcNewCountries = () => {
+    const listLength = countries.length;
+    return Math.min(countriesPerPage, listLength - cardsNumber);
+  };
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     data();
   }, [data]);
 
+  useEffect(() => {
+    if (!isIntersecting) return;
+    setCardsNumber((prev) => prev + calcNewCountries());
+  }, [isIntersecting]);
+
   if (status === 'loading') return <CardListSkeleton />;
   if (status === 'failed') return <CardListError error={error} />;
+
   return (
     <div className={css.wrapper}>
-      {countries.slice(0, 12).map((country, ind) => (
-        <CountryCard data={country} key={ind} />
-      ))}
+      {countries.slice(0, cardsNumber).map((country, ind) =>
+        ind === cardsNumber - 1 ? (
+          <div ref={node as RefObject<HTMLDivElement>} key={ind}>
+            <CountryCard data={country} />
+          </div>
+        ) : (
+          <CountryCard data={country} key={ind} />
+        )
+      )}
     </div>
   );
 };
