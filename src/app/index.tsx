e3705e39a97da1f7e0af598ceaf01c11aeee03b8
skyzavr <dankozero111@gmail.com>
2025-05-27 15:59:38 +0300

    [feat]: add providers and routes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,29 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import { NotFound } from '@pages/NotFound';
+import { HomePage } from '@pages/home';
+
+import { ThemeProvider } from '@features/theme/lib/ThemeProvider';
+
+import { MainLayout } from './layout/MainLayout';
+import { store } from './store/store';
+
 export const App = () => {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      element: <MainLayout />,
+      children: [
+        { path: '/', element: <HomePage /> },
+        { path: '*', element: <NotFound /> },
+      ],
+    },
+  ]);
+  return (
+    <ThemeProvider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </ThemeProvider>
+  );
 };
