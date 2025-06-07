654924d06e247b9090690ff448f5b0995aaed60e
skyzavr <dankozero111@gmail.com>
2025-06-07 21:38:38 +0300

    [feat]: reorganized note message based on screen width && add responsive styles

--- src/widgets/countryCard/ui/Card.tsx
+++ src/widgets/countryCard/ui/Card.tsx
@@ -12,15 +12,30 @@ export const Card = ({ country }: cardProps) => {
 
   const { borders, flags, correctBorders } = country;
 
+  const bordersIssue =
+    borders.length > correctBorders.length || correctBorders.length === 0;
+
+  const note = bordersIssue && (
+    <p className={css.note}>
+      You may be wondering where at least one neighbor is or if there should be
+      more neighbors, I know it for sure! Well, maybe we just don't have that
+      data :)
+    </p>
+  );
+
   return (
-    <div className={css.detailContainer}>
-      <div className={css.countryFlag}>
-        <img src={flags.svg} />
+    <>
+      <div className={css.detailContainer}>
+        <div className={css.countryFlag}>
+          <img src={flags.svg} />
+          <div className={css.smallScreenNote}>{note}</div>
+        </div>
+        <div className={css.countryWrapper}>
+          <DetailsList details={country} />
+          <CountryNeighbors correctBorders={correctBorders} />
+        </div>
       </div>
-      <div className={css.countryWrapper}>
-        <DetailsList details={country} />
-        <CountryNeighbors borders={borders} correctBorders={correctBorders} />
-      </div>
-    </div>
+      <div className={css.largeScreenNote}>{note}</div>
+    </>
   );
 };
