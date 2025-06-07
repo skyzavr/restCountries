dd56a64b0a54d69aa0c83c585d9c003cb5c4f2cd
skyzavr <dankozero111@gmail.com>
2025-06-07 22:37:51 +0300

    [fix]: removed unnecessary children element

--- src/shared/ui/button/Link.tsx
+++ src/shared/ui/button/Link.tsx
@@ -1,23 +1,17 @@
 import { Link } from 'react-router-dom';
-import { ReactElement } from 'react';
-import classNames from 'classnames';
 
 import css from './button.module.css';
 
 type btnProps = {
   title: string;
   to: string;
-  children?: ReactElement;
 };
 
 export const LinkButton = (props: btnProps) => {
-  const { title, children, to } = props;
+  const { title, to } = props;
 
-  const iconButton = children ? css.iconBtn : '';
-  const btnClassname = classNames(css.btnWrapper, iconButton);
   return (
-    <Link className={btnClassname} to={to}>
-      <span>{children}</span>
+    <Link className={css.btnWrapper} to={to}>
       {title}
     </Link>
   );
